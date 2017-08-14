7d10b92d18619319acc482d6760cc52d1ed2f0ce
AngelGordillo <angel.gordillodelgado@ext.entsoe.eu>
2017-08-14 17:08:18 +0200

    new error

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import Hapi from 'hapi';
 import Knex from '../knexfile';
 import jwt from 'jsonwebtoken';
+import routes from './routes'
+import GUID from 'node-uuid';
 const db = require( 'knex' )(Knex.development);
+const guid = GUID.v4();
 //create a new server instance
 const server = new Hapi.Server();
 
@@ -10,23 +13,33 @@ server.connection( {
 });
 // .register(...) registers a module within the instance of the API. The callback is then used to tell that the loaded module will be used as an authentication strategy. 
 server.register( require( 'hapi-auth-jwt' ), ( err ) => {
+
     server.auth.strategy( 'token', 'jwt', {
 
         key: 'vZiYpmTzqXMp8PpYXKwqc9ShQ1UhyAfy',
 
         verifyOptions: {
-     algorithms: [ 'HS256' ],
-        }
+           algorithms: [ 'HS256' ],
+       }
 
-    } );
+   } );
+    /* DOESN WORK ??????
+     // We move this in the callback because we want to make sure that the authentication module has loaded before we attach the routes. It will throw an error, otherwise. 
+      routes.forEach( ( route ) => {
+
+        console.log( `attaching ${ route.path }` );
+        server.route( route );
+
+    } );*/
 
 } );
 
+//SHOW WOMEN
 server.route({
 
     method: 'GET',
     path: '/women',
-        handler: ( request, reply ) => {
+    handler: ( request, reply ) => {
 
         const getOperation = db( 'women' ).select( '*' ).then( ( results ) => {
 
@@ -70,29 +83,35 @@ server.route( {
         .select( 'username', 'password' )
         .then( ( results ) => {
             const user = results[0];
-            console.log(user)
+           if( user.password === password && user.username === username ){
 
-    const token = jwt.sign( {
-        // You can have anything you want here. ANYTHING. As we'll see in a bit, this decoded token is passed onto a request handler.
-        username,
-        scope: user.username,
+            const token = jwt.sign( {
+            // You can have anything you want here. ANYTHING. As we'll see in a bit, this decoded token is passed onto a request handler.
+            username,
+            scope: user.username,
 
-    }, 'vZiYpmTzqXMp8PpYXKwqc9ShQ1UhyAfy', {
+        }, 'vZiYpmTzqXMp8PpYXKwqc9ShQ1UhyAfy', {
 
-        algorithm: 'HS256',
-        expiresIn: '1h',
+            algorithm: 'HS256',
+            expiresIn: '1h',
+
+        });
+
+            return reply({token,scope: user.username});
+        }else{
+            reply('wrong username or password');
+        }
 
     });
 
-    return reply({token, scope: user.name});
 
-        }).catch( ( err ) => {
 
-            reply(err);
 
-        } );
 
     }
+
+
+
 
 } );
 
@@ -109,3 +128,40 @@ server.start(err => {
     console.log( `Server started at ${ server.info.uri }` );
 
 });
+//  CREATE A WOMEN
+server.route( {
+
+    path: '/women',
+    method: 'POST',
+
+    handler: ( request, reply ) => {
+
+        const { women } = request.payload;
+
+    }
+
+} );
+
+
+
+ Knex( 'women' ).insert( {
+
+    owner: request.auth.credentials.scope,
+    name: women.name,
+    picture_url: bird.picture_url,
+    guid,
+
+} ).then( ( res ) => {
+
+    reply( {
+
+        data: guid,
+        message: 'successfully created bird'
+
+    } );
+
+} ).catch( ( err ) => {
+
+    reply( 'server-side error' );
+
+} );

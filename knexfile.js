9dd4a298b75d2fe46556386e5f13c0eadaa1593e
AngelGordillo <angel.gordillodelgado@ext.entsoe.eu>
2017-08-22 14:43:39 +0200

    change knex v1

--- knexfile.js
+++ knexfile.js
@@ -3,25 +3,25 @@ require('dotenv').config();
 module.exports = {
 
 	development: {
+
+			migrations: { tableName: 'knex_migrations' },
+			seeds: { tableName: './seeds' },
+			client: 'pg',
+			connection: process.env.DATABASE_URL + '?ssl=true&sslmode=require' || 'pg://angel1:123456@localhost:5432/women',	
+		  pool: {min: 2,max: 5}
+	       /* client: 'sqlite3',
+	        connection: {
+	        	filename: path.join(__dirname, 'db.sqlite')*/
+	        }
+
+    }
+/*	development: {
         client: 'pg',
         debug: true,
         connection: process.env.DATABASE_URL
-    }
-
-    }
+    }*/
 
 
-// development: {
-
-// 		migrations: { tableName: 'knex_migrations' },
-// 		seeds: { tableName: './seeds' },
-// 		client: 'pg',
-// 		connection: process.env.DATABASE_URL + '?ssl=true&sslmode=require' || 'pg://angel1:123456@localhost:5432/women',	
-// 	  pool: {min: 2,max: 5}
-//        /* client: 'sqlite3',
-//         connection: {
-//         	filename: path.join(__dirname, 'db.sqlite')*/
-//         }
 /*var knex = require('knex')({
   client: 'pg',
   version: '7.2',

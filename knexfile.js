ad83c23644289c769c4f82db6821a1a961d045b6
AngelGordillo <angel.gordillodelgado@ext.entsoe.eu>
2017-08-22 12:50:44 +0200

    heroku db

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,8 @@ module.exports = {
 		seeds: { tableName: './seeds' },
 		client: 'pg',
 		debug: true,
-		connection: process.env.DATABASE_URL || 'pg://angel1:123456@localhost:5432/women',
+		//connection: process.env.DATABASE_URL || 'pg://angel1:123456@localhost:5432/women',
+		connection: 'postgres://kwgxishsbflhir:53c5eed32140c7f4ac306d5d47bd1535646bb60eeb9ce2ee5f96ac472404c036@ec2-54-83-25-217.compute-1.amazonaws.com:5432/dcu5ge5vjntcuk',
 		searchPath: 'knex,public'
        /* client: 'sqlite3',
         connection: {

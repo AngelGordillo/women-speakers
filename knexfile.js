453a535c08d8135325f7d858c165509992d15e1d
AngelGordillo <angel.gordillodelgado@ext.entsoe.eu>
2017-08-22 14:59:16 +0200

    change knex v5

--- knexfile.js
+++ knexfile.js
@@ -7,9 +7,9 @@ module.exports = {
 			migrations: { tableName: 'knex_migrations' },
 			seeds: { tableName: './seeds' },
 			client: 'pg',
-			connection: 'postgres://iisqrfbzhbdobs:ad86925652584af73118347a47d6eadb47b41ce1b8a4477315620ab0c9db87c5@ec2-54-163-227-202.compute-1.amazonaws.com:5432/d4hupitm5nsu8',
+			//connection: 'postgres://iisqrfbzhbdobs:ad86925652584af73118347a47d6eadb47b41ce1b8a4477315620ab0c9db87c5@ec2-54-163-227-202.compute-1.amazonaws.com:5432/d4hupitm5nsu8',
 			ssl:true,
-			//connection: process.env.DATABASE_URL + '?ssl=true&sslmode=require' || 'pg://angel1:123456@localhost:5432/women',	
+			connection: process.env.DATABASE_URL + '?ssl=true&sslmode=require' || 'pg://angel1:123456@localhost:5432/women',	
 		  pool: {min: 2,max: 5}
 	       /* client: 'sqlite3',
 	        connection: {

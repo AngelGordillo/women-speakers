e89094d954844da0f76b9c0660650cdb0333b56b
AngelGordillo <angel.gordillodelgado@ext.entsoe.eu>
2017-08-22 14:25:40 +0200

    status

--- knexfile.js
+++ knexfile.js
@@ -2,19 +2,24 @@ var path = require('path');
 module.exports = {
 
 	development: {
-
-		migrations: { tableName: 'knex_migrations' },
-		seeds: { tableName: './seeds' },
-		client: 'pg',
-		connection: process.env.DATABASE_URL + '?ssl=true&sslmode=require' || 'pg://angel1:123456@localhost:5432/women',	
-	  pool: {min: 2,max: 5}
-       /* client: 'sqlite3',
-        connection: {
-        	filename: path.join(__dirname, 'db.sqlite')*/
-        }
+        client: 'pg',
+        connection: process.env.DATABASE_URL + '?ssl=true'
+    }
 
     }
 
+
+// development: {
+
+// 		migrations: { tableName: 'knex_migrations' },
+// 		seeds: { tableName: './seeds' },
+// 		client: 'pg',
+// 		connection: process.env.DATABASE_URL + '?ssl=true&sslmode=require' || 'pg://angel1:123456@localhost:5432/women',	
+// 	  pool: {min: 2,max: 5}
+//        /* client: 'sqlite3',
+//         connection: {
+//         	filename: path.join(__dirname, 'db.sqlite')*/
+//         }
 /*var knex = require('knex')({
   client: 'pg',
   version: '7.2',

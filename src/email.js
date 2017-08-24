5b820d306833e97e03cadaa39638a7bfa483be01
AngelGordillo <angel.gordillodelgado@ext.entsoe.eu>
2017-08-24 11:11:22 +0200

    email v1

---
+++ src/email.js
@@ -0,0 +1,15 @@
+var sendemail   = require('sendemail').email; // no api key
+var email = sendemail.email;
+sendemail.set_template_directory('./emails/welcome');
+
+var person = {
+  name : "Angel",
+  email: "angel.gordillodelgado@ext.entsoe.eu",
+  subject:"Welcome to test :)"
+}
+
+email('welcome', person, function(error, result){
+  console.log(' - - - - - - - - - - - - - - - - - - - - -> email sent: ');
+  console.log(result);
+  console.log(' - - - - - - - - - - - - - - - - - - - - - - - - - - - -')
+})

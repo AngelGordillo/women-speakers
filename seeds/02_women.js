6ea1207944d5d73842e5db046b60c327ee8af7c6
AngelGordillo <angel.gordillodelgado@ext.entsoe.eu>
2017-08-28 15:58:00 +0200

    seed

--- seeds/02_women.js
+++ seeds/02_women.js
@@ -12,8 +12,9 @@ exports.seed = function(knex, Promise) {
               picture_url: 'http://pngimg.com/upload/pigeon_PNG3423.png',
               topic: 'network', 
               linkedin:'default',
-              isPublic: true 
-              }
+              isPublic: false, 
+              owner: res[0]['id']
+            }
       ]);
     }); 
   })
